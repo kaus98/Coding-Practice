6a2c870a0a187973fde98b03dc9607dce7b2d623
Kaustubh Pathak <35065621+kaus98@users.noreply.github.com>
2022-11-27 21:25:29 +0530

    Create Jump Game 2-45-Medium.cpp

---
+++ Leetcode_Arrays/Jump Game 2-45-Medium.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        int max_jump = 1;
+        int MAXI = nums[0];
+        int temp = nums[0];
+        int target = nums.size() - 1;
+        for(int i = 1; i<target ; i++){
+            if(nums[i]+i > MAXI){
+                MAXI = nums[i]+i;
+            }
+            if(temp == i) {max_jump++;temp = MAXI;}
+            // cout<<"Steps : "<<steps << " Max_Jump : "<< max_jump << " MAXI : "<< MAXI <<endl;
+        }
+        return max_jump;
+    }
+};
