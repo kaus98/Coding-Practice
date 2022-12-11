348d3acb66dcf0b9755f67cb1d4369949b274596
Kaustubh Pathak <35065621+kaus98@users.noreply.github.com>
2022-12-11 15:14:25 +0000

    Added the Smallest Next Questions

--- Stack(Aditya_Verma)/NextSmallestRight.cpp
+++ Stack(Aditya_Verma)/NextSmallestRight.cpp
@@ -8,23 +8,23 @@
 
 using namespace std;
 
-vector<int> NextSmallestLeft(vector<int> arr){
+vector<int> NextSmallestRight(vector<int> arr){
   stack<int> st;
   vector<int> ans;
-  for(int i =0; i<arr.size(); i++) {
-    while(!st.empty() && st.top() <= arr[i]) st.pop();
+  for(int i = arr.size()-1; i>=0; i--) {
+    while(!st.empty() && st.top() >= arr[i]) st.pop();
     if(st.empty()) ans.push_back(-1);
     else ans.push_back(st.top());
     st.push(arr[i]);
   }
-  // reverse(ans.begin(), ans.end());
+  reverse(ans.begin(), ans.end());
   for(auto x : ans) cout<<x<<"\t";
-  // return ans;
+  return ans;
 }
 
 int main(){
   // Question is to find the Nearest bigger element next to Right Side.
   vector<int> arr{1,4,2,3,5,4,1,2,4,6,2,5,1,0,9,2,8,2,1,6,7,3,65,3,4,6,2,1,7,3,9};
-  NextSmallestLeft(arr);
+  vector<int>result = NextSmallestRight(arr);
   return 0;
 }
