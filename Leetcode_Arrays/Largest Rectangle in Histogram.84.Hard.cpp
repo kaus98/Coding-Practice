289400f29836e19d5dc1f9a0edc14841f065b05d
Kaustubh Pathak <35065621+kaus98@users.noreply.github.com>
2022-12-11 19:56:55 +0000

    Almost about to complete the course of Stack

---
+++ Leetcode_Arrays/Largest Rectangle in Histogram.84.Hard.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+vector<int> NextSmallestLeft(vector<int> arr){
+  stack<int> st;
+  vector<int> ans;
+  for(int i =0; i<arr.size(); i++) {
+    while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+    if(st.empty()) ans.push_back(-1);
+    else ans.push_back(st.top());
+    st.push(i);
+  }
+  // reverse(ans.begin(), ans.end());
+//   cout << "Printing Left Smallest : "<<endl;
+//   for(auto x : ans) cout<<x<<"\t";
+//   cout<<endl;
+  return ans;
+}
+
+vector<int> NextSmallestRight(vector<int> arr){
+  stack<int> st;
+  vector<int> ans;
+  for(int i = arr.size()-1; i>=0; i--) {
+    while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+    if(st.empty()) ans.push_back(arr.size());
+    else ans.push_back(st.top());
+    st.push(i);
+  }
+  
+  reverse(ans.begin(), ans.end());
+//   cout << "Printing Right Smallest : "<<endl;
+//   for(auto x : ans) cout<<x<<"\t";
+//   cout<<endl;
+  return ans;
+}
+
+
+int largestRectangleArea(vector<int> arr){
+  vector<int> NextR = NextSmallestRight(arr);
+  vector<int> NextL = NextSmallestLeft(arr);
+  int maximum = 0;
+
+  for(int i = 0; i < arr.size(); i++){
+    maximum = max(maximum, (NextR[i]-NextL[i]-1)*arr[i]);
+  }
+  return maximum;
+  
+}
+};
--- Stack(Aditya_Verma)/StockSpanProblem.cpp
+++ Stack(Aditya_Verma)/StockSpanProblem.cpp
@@ -8,23 +8,23 @@
 
 using namespace std;
 
-vector<int> NextSmallestLeft(vector<int> arr){
+vector<int> StockSpanProblem(vector<int> arr) {
   stack<int> st;
   vector<int> ans;
   for(int i =0; i<arr.size(); i++) {
-    while(!st.empty() && st.top() >= arr[i]) st.pop();
-    if(st.empty()) ans.push_back(-1);
-    else ans.push_back(st.top());
-    st.push(arr[i]);
+    while(!st.empty() && arr[st.top()] <= arr[i]) st.pop();
+    if(st.empty()) ans.push_back(i+1);
+    else ans.push_back(i - st.top());
+    st.push(i);
   }
-  // reverse(ans.begin(), ans.end());
+  
   for(auto x : ans) cout<<x<<"\t";
   return ans;
 }
 
 int main(){
   // Question is to find the Nearest bigger element next to Right Side.
-  vector<int> arr{1,4,2,3,5,4,1,2,4,6,2,5,1,0,9,2,8,2,1,6,7,3,65,3,4,6,2,1,7,3,9};
-  vector<int> ans = NextSmallestLeft(arr);
+  vector<int> arr{1,1,1,4,2,3,5,4,1,2,4,6,2,5,1,0,9,2,8,2,1,6,7,3,65,3,4,6,2,1,7,3,9};
+  vector<int>result = StockSpanProblem(arr);
   return 0;
 }
