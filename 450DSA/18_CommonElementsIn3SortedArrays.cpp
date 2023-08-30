0153bee328d1faf5dcb4596596839a28462652ed
Kaustubh Pathak <35065621+kaus98@users.noreply.github.com>
2023-08-30 18:08:49 +0530

    Final Push

--- 450DSA/18_CommonElementsIn3SortedArrays.cpp
+++ 450DSA/18_CommonElementsIn3SortedArrays.cpp
@@ -1,21 +1,78 @@
-#include <bits/stdc++.h> 
-vector<int> findCommonElements(vector<int> &a, vector<int> &b, vector<int> &c)
-{
-    // Write your code here
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    set<int> count;
-    while(i < a.size() && j < b.size() && k < c.size()){
-        if (a[i]==b[j] && b[j]==c[k]){
-            count.insert(a[i]);
-            i++;j++;k++;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int min1(int a, int b){
+            if(a>b) return b;
+            return a;
         }
-        else if (a[i] < min(b[j], c[k])) i++;
-        else if (b[j] < min(a[i], c[k])) j++;
-        else k++;
+        
+        int min3(int a, int b, int c){
+            if(a==b && a>c) return 2;
+            if(a==b && a<c) return 1;
+            if(a<b && a<c) {return 0;}
+            if(b<a && b<c) {return 1;}
+            return 2;
+            
+        }
+       vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
+        {
+            //code here.
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            set<int> count;
+            while((i < n1) && (j < n2) && (k < n3)){
+                
+                if (a[i]==b[j] && b[j]==c[k]){
+                    count.insert(a[i]);
+                    i++;j++;k++;
+                    //cout<<"Inserting the Element Here : "<<a[i]<<endl;
+                }
+                else{
+                    int temp = min3(a[i], b[j], c[k]);
+                    
+                    if(temp == 0) i++;
+                    else if(temp == 1) j++;
+                    else k++;
+                }
+            }
+            vector<int> count1(count.begin(), count.end());
+            return count1;
+        }
+
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
     }
-    vector<int> count1(count.begin(), count.end());
-    return count1;
 }
+// } Driver Code Ends
--- 450DSA/9_MinimumNumberToReachEnd.cpp
+++ 450DSA/9_MinimumNumberToReachEnd.cpp
@@ -1,25 +1,18 @@
-int minimumJumps(vector<int> &arr, int n)
-{
-    // write your code here
-
-    if(n == 1) return 0;
-    if(arr[0] == 0) return -1;
-    int target = n-1;
-    int start = 0;
-    int temp = arr[0];
-    int maximax = arr[0];
-    int jumps = 1;
-    for(int i = 1; i<n-1;i++){
-        maximax = max(maximax, arr[i]+i);
-
-        if (temp == i){
-            jumps++;
-            if(maximax == temp) return -1;
-            temp = maximax;
-
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        int max_jump = 1;
+        int MAXI = nums[0];
+        int temp = nums[0];
+        int target = nums.size() - 1;
+        for(int i = 1; i<target ; i++){
+            if(nums[i]+i > MAXI){
+                MAXI = nums[i]+i;
+            }
+            if(temp == i) {max_jump++;temp = MAXI;}
+            // cout<<"Steps : "<<steps << " Max_Jump : "<< max_jump << " MAXI : "<< MAXI <<endl;
         }
-
-
+        return max_jump;
     }
-    return jumps;
-}   
+}
