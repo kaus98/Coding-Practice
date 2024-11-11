d1d19f83f432687bef0507a085d843a4711730df
Kaustubh Pathak <35065621+kaus98@users.noreply.github.com>
2024-11-12 00:58:48 +0530

    Maximum Average Subarray I-643-Easy.cpp

---
+++ Leetcode/SlidingWindow/Maximum Average Subarray I-643-Easy.cpp
@@ -0,0 +1,46 @@
+// You are given an integer array nums consisting of n elements, and an integer k.
+
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+// Example 1:
+
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+// Example 2:
+
+// Input: nums = [5], k = 1
+// Output: 5.00000
+
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double totalSum = 0;
+        double maxAverage = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while( j < k ){
+            totalSum += nums[j];
+            j++;
+        }
+
+        maxAverage = totalSum;
+
+        while(j < nums.size()){
+            totalSum += nums[j];
+            totalSum -= nums[i];
+            j++;
+            i++;
+
+            maxAverage = max(maxAverage, totalSum);
+
+        }
+
+        return maxAverage/k;
+
+    }
+};
